4cfafab7b2b3974457e1a3150ef4cf7d02347acc
Neha Jagtap <44936257+nehajgtp@users.noreply.github.com>
2019-10-30 21:58:45 -0400

    Add files via upload

---
+++ Assignment_2/lex.cpp
@@ -0,0 +1,263 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <cctype>
+#include <string>
+#include <map>
+#include "lex.h"
+
+using namespace std;
+
+std::map<Token, string> print_token = {
+    {PRINT, "PRINT"}, {LET, "LET"}, {IF, "IF"}, {LOOP, "LOOP"}, {BEGIN, "BEGIN"}, {END, "END"}, {ID, "ID"}, {INT, "INT"}, {STR, "STR"}, {PLUS, "PLUS"}, {MINUS, "MINUS"}, {STAR, "STAR"}, {SLASH, "SLASH"}, {BANG, "BANG"}, {LPAREN, "LPAREN"}, {RPAREN, "RPAREN"}, {SC, "SC"}, {ERR, "ERR"}, {DONE, "DONE"}
+
+};
+
+ostream &operator<<(ostream &out, const Lex &tok)
+{
+    Token tt = tok.GetToken();
+
+    string lm = tok.GetLexeme();
+
+    if (tt == ID || tt == INT || tt == STR)
+    {
+        out << print_token[tt] << "(" << lm << ")";
+    }
+
+    if (tt == PLUS || tt == MINUS || tt == BANG || tt == STAR || tt == SLASH || tt == SC || tt == IF || tt == PRINT || tt == LOOP || tt == BEGIN || tt == END || tt == LET)
+    {
+        out << print_token[tt];
+    }
+    return out;
+}
+
+static map<string, Token> kwmap = {{"if", IF}, {"print", PRINT}, {"loop", LOOP}, {"begin", BEGIN}, {"end", END}, {"let", LET}};
+
+Lex idkw(string lexeme, int linenum)
+{
+    Token tt = ID;
+    auto kIt = kwmap.find(lexeme);
+    if (kIt != kwmap.end()) {
+        tt = kIt->second;
+    }
+        
+    
+    return Lex(tt, lexeme, linenum);
+}
+
+Lex getNextToken(istream &in, int &linenum)
+{
+    enum LexState
+    {
+        BEGIN,
+        IN_IDENTIFIER,
+        POSSIBLE_NEGATIVE,
+        IN_INTEGER,
+        IN_STRING,
+        SAW_BACKSLASH,
+        IN_COMMENT
+    } lexstate = BEGIN;
+    string lexeme;
+    char c;
+
+    while (in.get(c))
+    {
+
+        if (c == '\n')
+        {
+            linenum++;
+        }
+
+        switch (lexstate)
+        {
+        case BEGIN:
+            if (isspace(c))
+            {
+                continue;
+            }
+
+            else if (isdigit(c))
+            {
+                lexeme += c;
+                lexstate = IN_INTEGER;
+            }
+            else if (c == '/')
+            {
+                if (in.peek() == '/')
+                {
+                    lexstate = IN_COMMENT;
+                }
+
+                else
+                {
+                    return Lex(SLASH, "/", linenum);
+                }
+            }
+
+            else if (c == '"')
+            {
+                lexeme += c;
+                lexstate = IN_STRING;
+            }
+            else if (isalpha(c))
+            {
+                lexeme += c;
+                lexstate = IN_IDENTIFIER;
+            }
+
+            else if (c == '-')
+            {
+                lexstate = POSSIBLE_NEGATIVE;
+            }
+
+            else if (c == '+')
+            {
+                return Lex(PLUS, "+", linenum);
+            }
+
+            else if (c == '*')
+            {
+                return Lex(STAR, "*", linenum);
+            }
+
+            else if (c == '!')
+            {
+                return Lex(BANG, "!", linenum);
+            }
+
+            else if (c == '(')
+            {
+                return Lex(LPAREN, "(", linenum);
+            }
+
+            else if (c == ')')
+            {
+                return Lex(RPAREN, ")", linenum);
+            }
+
+            else if (c == ';')
+            {
+                return Lex(SC, ";", linenum);
+            }
+
+            else if (in.eof())
+            {
+                return Lex(DONE, "DONE", linenum);
+            }
+
+            else
+            {
+                lexeme += c;
+                return Lex(ERR, lexeme, linenum);
+            }
+            break;
+
+        case IN_IDENTIFIER:
+            if (isalnum(c))
+            {
+                lexeme += c;
+            }
+
+            else
+            {
+                if (c == '\n')
+                {
+                    linenum--;
+                }
+                in.putback(c);
+                return idkw(lexeme, linenum);
+            }
+            break;
+        case POSSIBLE_NEGATIVE:
+            if (isdigit(in.peek()))
+            {
+                lexstate = IN_INTEGER;
+            }
+
+            else
+            {
+                in.putback(c);
+                if (c == '\n')
+                {
+                    linenum--;
+                }
+                return Lex(MINUS, "-", linenum);
+            }
+            break;
+        case IN_STRING:
+            if (c == '\\')
+            {
+                lexstate = SAW_BACKSLASH;
+            }
+
+            else if (in.eof() || (c == '\n'))
+            {
+                lexeme += c;
+                linenum--;
+                return Lex(ERR, lexeme, linenum);
+            }
+
+            else if (c == '"')
+            {
+                lexeme = lexeme.substr(1, lexeme.length()-1);
+                return Lex(STR, lexeme, linenum);
+            }
+
+            else
+            {
+                lexeme += c;
+                continue;
+            }
+
+            break;
+
+        case SAW_BACKSLASH:
+            if (c == 'n')
+            {
+                lexeme += '\n';
+                lexstate = IN_STRING;
+            }
+                
+            else
+            {
+                lexeme += c;
+                lexstate = IN_STRING;
+            }
+       
+        break;
+                
+        case IN_INTEGER:
+            if (isdigit(c))
+            {
+                lexeme += c;
+            }
+
+            else
+            {
+                in.putback(c);
+                if (c == '\n')
+                {
+                    linenum--;
+                }
+                return Lex(INT, lexeme, linenum);
+            }
+            break;
+
+        case IN_COMMENT:
+            if (c == '\n')
+            {
+                lexstate = BEGIN;
+            }
+
+            else
+            {
+                continue;
+            }
+            break;
+       }
+    }
+
+    if (in.eof())
+        return Lex(DONE, "DONE", linenum);
+    return Lex(ERR, lexeme, linenum);
+}
---
+++ Assignment_2/main.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <fstream>
+using std::ifstream;
+#include <string>
+#include <iterator>
+#include <map>
+#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include "lex.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    int lineNumber = 0;
+    istream *in = &cin;
+    ifstream file;
+    bool vflag = false;
+    bool idsflag = false;
+    bool constsflag = false;
+    bool constslabel_str = false;
+    bool constslabel_int = false;
+    bool onefile = false;
+    bool cannotopen = false;
+    bool unrecognizedflag = false;
+    int tokCount = 0;
+    map<string, bool> ids;
+    vector<string> strings;
+    vector<string> integers;
+
+    for (int i = 1; i < argc; i++)
+    {
+        string arg(argv[i]);
+        if (arg == "-v")
+            vflag = true;
+        else if (arg == "-ids")
+            idsflag = true;
+        else if (arg == "-consts") 
+            constsflag = true;
+        else if (arg[0] == '-')
+        {
+            cout << "UNRECOGNIZED FLAG " << arg << endl;
+            unrecognizedflag = true;
+        }
+        else if (in != &cin)
+        {
+            cout << "ONLY ONE FILE NAME ALLOWED" << endl;
+            onefile = true;
+        }
+        else
+        {
+            file.open(arg);
+            if (file.is_open() == false)
+            {
+                cout << "CANNOT OPEN " << arg << endl;
+                cannotopen = true;
+            }
+
+            in = &file;
+        }
+    }
+
+    if (onefile == false && cannotopen == false && unrecognizedflag == false)
+    {
+        Lex l;
+        string iden = "IDENTIFIERS: ";
+        int iden_count = 0;
+
+        while (((l = getNextToken(*in, lineNumber)).GetToken()) != DONE && l != ERR)
+        {
+            
+            if (vflag)
+            {
+                cout << l << endl;
+            }
+            tokCount++;
+            if (idsflag)
+            {
+                if (l.GetToken() == ID)
+                {
+                    ids[l.GetLexeme()] = true;
+                    iden_count++;
+                }
+            }
+            
+            if (constsflag) {
+                if (l.GetToken() == STR) {
+                    constslabel_str = true;
+                    strings.push_back(l.GetLexeme());
+                }
+                
+                if (l.GetToken() == INT) { 
+                    constslabel_int = true;
+                    integers.push_back(l.GetLexeme());
+                }
+                
+            }
+        }
+
+        sort(integers.begin(), integers.end(), greater<string>());
+        sort(strings.begin(), strings.end());
+        
+        if (idsflag == true && iden_count > 0)
+        {
+            cout << iden;
+            auto it = ids.begin();
+            cout << it->first;
+
+            for (it++; it != ids.end(); it++)
+                cout << ", " << it->first;
+            cout << endl;
+        }
+        
+
+        if (l.GetToken() == ERR)
+        {
+            cout << "Error on line " << (l.GetLinenum() +1) << " (" << l.GetLexeme() << ")" << endl;
+        }
+
+        if (constslabel_str && l.GetToken() == DONE) {
+            cout << "STRINGS:" << endl;
+            for (string x : strings) 
+                cout << x << endl;
+        }
+        
+        if (constslabel_int && l.GetToken() == DONE) {
+            cout << "INTEGERS:" << endl; 
+            for (auto y : integers) 
+                cout << y << endl;
+        }
+        
+        if (l.GetToken() == DONE)
+        {
+            cout << "Lines: " << l.GetLinenum() << endl;
+            if (getNextToken(*in, lineNumber).GetLinenum() > 0)
+            {
+                cout << "Tokens: " << tokCount << endl;
+            }
+        }
+    }
+}
